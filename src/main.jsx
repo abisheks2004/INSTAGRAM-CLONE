88fa111d11fc027fe89758326fc32600dc5fcbd5
abisheks2004 <abisheka067@gmail.com>
2025-05-25 12:22:20 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,27 @@
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ViewStory from './ViewStory.jsx'
 import Profile from './Profile.jsx'
 
+const router = createBrowserRouter(
+[
+{
+  path:'/',
+  element:<App/>
+},
+{
+  path :'/story/:id/:tot',
+  element:<ViewStory/>
+},
+{
+ path :'/profile',
+  element:<Profile/>
+}
+]
+)
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/INSTAGRAM-CLONE">
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/story/:id/:tot" element={<ViewStory />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </HashRouter>
-);
+
+  <RouterProvider router={router} />
+)
