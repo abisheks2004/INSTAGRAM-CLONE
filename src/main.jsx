6ae624947fd5974df6301accd9257dd622a896ab
abisheks2004 <abisheka067@gmail.com>
2025-05-25 15:47:12 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,39 +3,24 @@ import App from './App.jsx'
 import ViewStory from './ViewStory.jsx'
 import Profile from './Profile.jsx'
 import NotFound from './NotFound.jsx'
-import Feed from './Feed.jsx'  // <-- Add this import
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Feed from './Feed.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Feed />
-      },
-      {
-        path: 'story/:id/:tot',
-        element: <ViewStory />
-      },
-      {
-        path: 'profile',
-        element: <Profile />
-      },
-      {
-        path: '*',
-        element: <NotFound />
-      }
-    ]
-  },
-  {
-    path: '*',
-    element: <NotFound />
-  }
-])
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Feed />} />
+        <Route path="story/:id/:tot" element={<ViewStory />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </HashRouter>
 )
