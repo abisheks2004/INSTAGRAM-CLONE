90102b0f1d14a71966dd5851eaa03764757c3f04
abisheks2004 <abisheka067@gmail.com>
2025-05-25 11:44:03 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,27 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ViewStory from './ViewStory.jsx'
 import Profile from './Profile.jsx'
 
-function ErrorPage() {
-  return (
-    <div style={{ padding: 20, textAlign: 'center' }}>
-      <h1>Oops! Something went wrong.</h1>
-      <p>The page you are looking for doesn't exist or an error occurred.</p>
-    </div>
-  )
+const router = createBrowserRouter(
+[
+{
+  path:'/',
+  element:<App/>
+},
+{
+  path :'/story/:id/:tot',
+  element:<ViewStory/>
+},
+{
+ path :'/profile',
+  element:<Profile/>
 }
-
-function NotFound() {
-  return (
-    <div style={{ padding: 20, textAlign: 'center' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you requested does not exist.</p>
-    </div>
-  )
-}
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />, // handles component errors
-    children: [
-      { path: 'story/:id/:tot', element: <ViewStory /> },
-      { path: 'profile', element: <Profile /> },
-      { path: '*', element: <NotFound /> }, // handles unmatched routes (404)
-    ],
-  },
-])
-
+]
+)
 createRoot(document.getElementById('root')).render(
+
   <RouterProvider router={router} />
 )
