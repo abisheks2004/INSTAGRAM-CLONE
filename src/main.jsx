8872b0d78626ab0bb89dee47da71b598bbf7d587
abisheks2004 <abisheka067@gmail.com>
2025-05-25 11:38:54 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,23 +5,31 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ViewStory from './ViewStory.jsx'
 import Profile from './Profile.jsx'
 
-const router = createBrowserRouter(
-[
-{
-  path:'/',
-  element:<App/>
-},
-{
-  path :'/story/:id/:tot',
-  element:<ViewStory/>
-},
-{
- path :'/profile',
-  element:<Profile/>
+function ErrorPage() {
+  return (
+    <div style={{ padding: 20, textAlign: 'center' }}>
+      <h1>Oops! Something went wrong.</h1>
+      <p>The page you are looking for doesn't exist or an error occurred.</p>
+    </div>
+  )
 }
-]
-)
-createRoot(document.getElementById('root')).render(
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,  // ← Add this line here
+  },
+  {
+    path: '/story/:id/:tot',
+    element: <ViewStory />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+])
+
+createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 )
